88ea99087a64fdb5010481586cfa367c2099f2cf
Poikamon <oikawapedro@gmail.com>
2025-11-23 19:00:31 -0300

    Estilização por css

--- MathBox.py
+++ MathBox.py
@@ -1,18 +1,35 @@
 #Import
-import RPi.GPIO as GPIO
+
+import streamlit as st
 import random
 import time
+
+with open("styles.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+#Variáveis 
+
+player = 0
+placeholder = st.empty()
 
 #Função
 
 def MathBox():
     '''Função que escolhe um valor aleatório e compara com um resultado'''
-    player = 0
     exvalue = random.randint(-27,27) #Pega um valor possível para a caixa
-    print(exvalue)
+    placeholder.markdown(
+        f"""
+        <div class='bloco-valor'>
+            {exvalue}
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+    time.sleep(2)
 
     MathBox = [] #Lista dos cubos dentro da caixa
 
+    placeholder.markdown("<div class='titulo'>Primeiro número:</div>", unsafe_allow_html=True)
+    
     cube1 = input(' numero:')
     a = 0
 
@@ -53,15 +70,16 @@ def MathBox():
             a = -8
         case 'E2801190A503006242D77717':
             a = -9
-        case _: 
-            error = 'Valor inválido'
-            
+        case _:
+            print("valor inválido")
+
     MathBox.append(a) #Adiciona valores escolhidos à lista da caixa
     print(a)
-
+    placeholder.markdown("<div class='titulo'>Segundo número:</div>", unsafe_allow_html=True)
+    
     cube2 = input(' numero:')
     b = 0
-
+    
     match cube2:
         case 'E2801190A503006242D77787':
             b = 1
@@ -99,7 +117,9 @@ def MathBox():
             b = -8
         case 'E2801190A503006242D77717':
             b = -9
-            
+        case _:
+            print("valor inválido")
+    placeholder.markdown("<div class='titulo'>Terceiro número:</div>", unsafe_allow_html=True)   
     MathBox.append(b) #Adiciona valores escolhidos à lista da caixa
     print(b)
 
@@ -143,41 +163,66 @@ def MathBox():
             c = -8
         case 'E2801190A503006242D77717':
             c = -9
-            
+        case _:
+            print("valor inválido")
+
     MathBox.append(c) #Adiciona valores escolhidos à lista da caixa
     print(c)
 
     value = sum(MathBox) #soma os valores da caixa
 
     if value == exvalue:
-        print("Resposta correta :)")
+        placeholder.markdown("<div class='correto'>Resposta correta :)</div>", unsafe_allow_html=True)
         player += 1
     else:
-        print("Resposta incorreta :(")  #compara o valor encontrado com o esperado
+        placeholder.markdown("<div class='errado'>Resposta incorreta :(</div>", unsafe_allow_html=True)
 
+#Código principal
 
+placeholder.markdown(
+    """
+    <h1 class='titulo-principal'>
+        Aperte o botão e teste sua habilidade matemática!
+    </h1>
+    """,
+    unsafe_allow_html=True
+)
 
+op = input()
+
+if op == 's':
+    timer = time.time() + 100 #1 minuto :3
+
+    while time.time() < timer:
+        MathBox()
+        time.sleep(2)
+    print('acabou o tempo!')
+    player = 0
+# O código abaixo é para rodar no Raspberry Pi com o botão físico
         
 #Código principal
         # Configurar a numeração dos pinos
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setwarnings(False)
 
 # Definir o número do pino do botão
-botao_pin = 3
+#botao_pin = 3
 # Configurar o pino do botão como entrada com pull-up interno (o botão é conectado entre o pino e o terra)
-GPIO.setup(botao_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(botao_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-timer = time.time() + 120
+#timer = time.time() + 120
 
-try:
-    while True:
+#try:
+    #while True:
         # Ler o estado do botão
         # Se o botão estiver pressionado, a leitura será baixa (LOW)
-        if GPIO.input(botao_pin) == GPIO.LOW:
-            while time.time()<timer:
-                MathBox()
-except KeyboardInterrupt:
-    print("Programa encerrado.")
-finally:
-    GPIO.cleanup()
+        #if GPIO.input(botao_pin) == GPIO.LOW:
+            #while time.time()<timer:
+                #MathBox()
+#except KeyboardInterrupt:
+    #print("Programa encerrado.")
+#finally:
+    #GPIO.cleanup()
+
+
+
--- interface.py
+++ interface.py
@@ -5,4 +5,3 @@ st.set_page_config(page_title="Math Box", page_icon="🧮", layout="centered")
 st.title("Math Box - Interface")
 st.write("Bem-vindo! Digite os números para calcular:")
 
-
